652900cdcec977bb40b7f05876f71134542433cb
sxq12320 <3383615693@qq.com>
2026-03-25 13:53:24 +0800

    test_caomei

---
+++ 3_ultralytics-main/000_test.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# 加载模型
+model = YOLO(r"E:\mastercode\3_ultralytics-main\results\segment\1_caomei_test\train4\weights\best.pt")
+
+# 预测
+results = model.predict(
+    source="E:/mastercode/data/caomei/final/images/val",   # 图片或文件夹
+    save=True,              # 保存结果
+    conf=0.50,               # 置信度阈值
+    project=r"E:\mastercode\3_ultralytics-main\results\segment\1_caomei_test\train4",   # ← 你想保存的根目录
+    name="predict_val"
+)
+
+# 查看结果
+for r in results:
+    print(r.masks)   # 分割结果
---
+++ 3_ultralytics-main/002_kvasir.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    yolo = YOLO(r'E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\yolo11n-seg-sxq_2.yaml')
+    yolo.train(
+        data=r'E:\mastercode\3_ultralytics-main\203_kvasir_data.yaml',
+        project=r'E:\mastercode\3_ultralytics-main\results\segment\2_kvasir_test',
+        name='yolo11_improve_2',
+        epochs=200,
+        imgsz=640,
+        batch=8,
+        lr0=0.0001,
+        momentum=0.937,
+        weight_decay=0.0005,
+        optimizer='AdamW',
+        amp = False,
+        cache = True
+    )
