4b9af7789500b397d5829a064db439352fbf91fa
sxq12320 <3383615693@qq.com>
2026-04-01 18:01:02 +0800

    开始进行yolo11n_half_channel 训练

--- 3_ultralytics-main/005_jeruk_test.py
+++ 3_ultralytics-main/005_jeruk_test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\yolo11n-seg.yaml") # 原始的yolo11n架构，通道数量不变
-    yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\yolo11n-seg.yaml") # 将原始的通道数量降低一倍，看看效果
+    yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\yolo11n-seg.yaml") # 将原始的通道数量降低一半
     yolo.train(
         data=r'E:\mastercode\3_ultralytics-main\205_jeurk_spilt_data.yaml',
         project=r'E:\mastercode\3_ultralytics-main\results\segment\4_jeurk_test_mini',
