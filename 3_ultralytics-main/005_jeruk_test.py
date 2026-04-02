4a41037a2dbaffc35d01e3a2d6e7323539107730
sxq12320 <3383615693@qq.com>
2026-04-02 19:53:39 +0800

    LSNET进入训练完成

--- 3_ultralytics-main/005_jeruk_test.py
+++ 3_ultralytics-main/005_jeruk_test.py
@@ -4,11 +4,13 @@ if __name__ == '__main__':
     # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\yolo11n-seg.yaml") # 原始的yolo11n架构，通道数量不变
     # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\1_yolo11n-seg-halfchannel.yaml") # 将原始的通道数量降低一半
     # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\2_yolo11n-seg-DWCONV.yaml") # 使用原来的架构同时将其换成深度可分离卷积结构
-    yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\3_yolo11n-seg-DWany.yaml") # 将全部的卷积都替换成深度可分离卷积，包括里面的那个C3K2模块也替换掉
+    # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\3_yolo11n-seg-DWany.yaml") # 将全部的卷积都替换成深度可分离卷积，包括里面的那个C3K2模块也替换掉
+    # yolo = YOLO(r"E:\mastercode\3_ultralytics-main\ultralytics\cfg\models\11\4_yolo11n-seg-LSNET.yaml") # 将C3K2模块编程LSnet的模块，看看效果
+    
     yolo.train(
         data=r'E:\mastercode\3_ultralytics-main\205_jeurk_spilt_data.yaml',
         project=r'E:\mastercode\3_ultralytics-main\results\segment\4_jeurk_test_mini',
-        name='4-yolo11n_DWany',
+        name='5-yolo11n_LSNET',
         epochs=200,
         imgsz=640,
         batch=8,
