19467e67c42b0f8b8d9f4dbee29670a209432924
sxq12320 <3383615693@qq.com>
2026-04-15 18:56:11 +0800

    FWQ代码更新

--- 5_FWQ/ultralytics-main/006_Apple_Amodal_test.py
+++ 5_FWQ/ultralytics-main/006_Apple_Amodal_test.py
@@ -5,36 +5,33 @@ if __name__ == '__main__':
      
     '''
     yolo = YOLO(r"ultralytics/cfg/models/11/yolo11n-seg.yaml") 
-        RGB three channels and a Depth Channel with the basic yolo11n which is not changed
+        1.使用最基本的yolo11nano架构,仅仅在网络输入部分增加一个深度信息,深度信息的样式为PNG格式
 
     yolo = YOLO(r"ultralytics-main/ultralytics/cfg/models/11/1_yolo11-seg-DWconv.yaml")
-        RGB three channels and a depth channel with the basic yolo11n which is changed Conv modules in the backbone but not changed the C3K2 module
+        2.在上面的基础之上,修改yolo11nano卷积效果,将原本的普通卷积变为现在的深度卷积+逐点卷积并进行运算
     
     yolo = YOLO(r"ultralytics-main/ultralytics/cfg/models/11/2_yolo11-seg-DWAny.yaml")
-        RGB and Depth four channels with the basic architechure yolo11n  which is changed C3K2 module with the C3K2_DW.
-        In the C3K2_DW, all Conv module were replaced with depth wide Conv and pointed wide Conv
+        3.在上面的基础之上,不仅仅修改普通的卷积,而且将C3K2模块内部的卷积全部替换成深度卷积+逐点卷积进行运算
     
     yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/3_yolo11-seg-RGBD_C3K2LS.yaml")
-        using the module from some article named "See large attention small".then upated the C3K2 module from the basic to C3K2_LS.
-        In mean time , we using the yolo 11 nano backbone to train this amodal segmentation tasks 
+        4.在论文看大注意小的基础之上,我们引入了LSNET的架构,同时将LSNET和C3K2架构融合在一起并将其命名为C3K2_LS,在其他不发生修改的情况之下仅仅对模块进行替换
     
     yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/4_yolo11-seg-Dense-skiplink.yaml")
-        using my famous Backbone which named Dense Skip .this backbone is added a C3K2 module in the basic yolo11 backbone.
-        in the mean time, we using the such resnet to concat feature better
+        5.直接就是修改yolo11nano的主干网络架构模型,引入自己创造的DenseSkip连接思路,并且将这个思路运用在主干网络之中实现带有层注意力的跳跃连接
 
+    yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/5_yolo11_seg_Dense_and_C3K2LS.yaml")
+        6.在上面的基础之上,也就是已经做过主干修改DenseSkip后东西,将其中的C3K2模块替换成C3K2_LS模块并进行运算
     
         
     '''
     yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/5_yolo11_seg_Dense_and_C3K2LS.yaml")
-    # yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/4_yolo11-seg-Dense-skiplink.yaml")
-    # yolo = YOLO(r"code/ultralytics-main/ultralytics/cfg/models/11/3_yolo11-seg-RGBD_C3K2LS.yaml")
     yolo.train(
         data=r'206_Apple_Amodal.yaml',
         project=r'/data/sxq/code/ultralytics-main/results',
         name='5_yolo11n-seg-Dense-and-C3K2_ls',
         epochs=500,
         imgsz=640,
-        batch=4,
+        batch=32,
         lr0=0.0001,
         momentum=0.9,
         weight_decay=0.0005,
@@ -103,7 +100,7 @@ if __name__ == '__main__':
 #         max_det=100,
 #         # 严防死守色彩增强
 #         # --- 新增修改点：关闭多图增强操作 ---
-#         mosaic=0.0,  # 关闭马赛克增强（避免向空 buffer 索要数据，同时保护深度图几何特征）
+#         mosaic=0.0,  # 关闭马赛克增强（避免向空 buffer 索要数据,同时保护深度图几何特征）
 #         mixup=0.0,   # 关闭 MixUp 增强
 #         copy_paste=0.0, # 关闭复制粘贴增强（同理）
 #         hsv_h=0.0, 
