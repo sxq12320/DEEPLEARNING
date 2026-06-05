e4719b3f24b108b8edd5f796d40b8f2520aa801c
sxq12320 <3383615693@qq.com>
2026-06-05 14:02:52 +0800

    adding some tips

--- ultralytics-main-new/006_Apple_Amodal_test.py
+++ ultralytics-main-new/006_Apple_Amodal_test.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
          data=r"E:\mastercode\ultralytics-main-new\206_Apple_Amodal.yaml",
          project=r"E:\mastercode\ultralytics-main-new\results",
          name="01_yolo11n-seg-base-rgbd_fix",
-         optimizer="AdamW",
+         optimizer="PIDAO",
          epochs=20,
          patience=50,
          imgsz=400,
@@ -31,4 +31,4 @@ if __name__ == "__main__":
          cache=False,  
          seed=SEED,
          amp = 0,
-    )
+      )
