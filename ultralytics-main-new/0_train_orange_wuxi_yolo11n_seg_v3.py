6445d14dd3a64d356226840572f8418f5a7560d7
sxq12320 <3383615693@qq.com>
2026-07-13 12:20:46 +0800

    mobilenetv4 backbone

--- ultralytics-main-new/0_train_orange_wuxi_yolo11n_seg_v3.py
+++ ultralytics-main-new/0_train_orange_wuxi_yolo11n_seg_v3.py
@@ -16,11 +16,12 @@ torch.backends.cudnn.benchmark = False
 
 if __name__ == "__main__":
     # yolo = YOLO(r"E:\mastercode\ultralytics-main-new\0_orange_yaml\001_yolo11-seg.yaml")
-    yolo = YOLO(r"E:\mastercode\ultralytics-main-new\0_orange_yaml\002_yolo11-seg-starnet.yaml")
+    # yolo = YOLO(r"E:\mastercode\ultralytics-main-new\0_orange_yaml\002_yolo11-seg-starnet.yaml")
+    yolo = YOLO(r"E:\mastercode\ultralytics-main-new\0_orange_yaml\003_yolo11-seg-mobilenetv4.yaml")
     yolo.train(
         data=r"E:\mastercode\ultralytics-main-new\200orange_wuxi_seg.yaml",
         project=r"E:\mastercode\ultralytics-main-new\1_results",
-        name="002_yolo11n_starnet_seg_AdamW",
+        name="003_yolo11n_mobilenetv4_seg_AdamW",
         optimizer="AdamW",
         epochs=300,
         patience=100,
